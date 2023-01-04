e1077560769c90124118c5ca0d57a2d7c82a1299
leezer94 <2kunhee94@gmail.com>
2023-01-04 21:41:55 +0900

    Feat : issueList에 대한 apiSlice 추가

---
+++ src/apis/apiSlice.js
@@ -0,0 +1,39 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const apiSlice = createApi({
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3001' }),
+  endpoints: builder => ({
+    getTodos: builder.query({
+      query: () => '/issueList',
+      transformResponse: todos => todos.sort((a, b) => b.id - a.id),
+      providesTags: ['Todos'],
+    }),
+    addTodos: builder.mutation({
+      query: todo => ({
+        url: '/issueList',
+        method: 'POST',
+        body: todo,
+      }),
+      invalidatesTags: ['Todos'],
+    }),
+    updateTodo: builder.mutation({
+      query: todo => ({
+        url: `/issueList/${todo.id}`,
+        method: 'PATCH',
+        body: todo,
+      }),
+      invalidatesTags: ['Todos'],
+    }),
+    deleteTodo: builder.mutation({
+      query: id => ({
+        url: `/issueList/${id}`,
+        method: 'DELETE',
+        body: id,
+      }),
+      invalidatesTags: ['Todos'],
+    }),
+  }),
+});
+
+export const { useGetTodosQuery, useAddTodosMutation, useUpdateTodoMutation, useDeleteTodoMutation } = apiSlice;
