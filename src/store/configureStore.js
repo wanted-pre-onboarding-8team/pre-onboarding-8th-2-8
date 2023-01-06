bfdc10183c4cb43262542aec449d241119e34538
leezer94 <2kunhee94@gmail.com>
2023-01-06 18:36:46 +0900

    Feat : rtk Query 적용 및 server script 추가

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,7 +1,9 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { apiSlice } from 'apis/apiSlice';
 import issueSlice from 'slices/issueSlice';
 
 const rootReducers = combineReducers({
+  [apiSlice.reducerPath]: apiSlice.reducer,
   issue: issueSlice.reducer,
 });
 
